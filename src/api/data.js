6fc1fe5798819b10dcb921c5b6d3ece79b2f4d80
DamienL47 <damien.lataste@lapiscine.pro>
2023-10-16 14:18:19 +0200

    fix style dashBoard

--- src/api/data.js
+++ src/api/data.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "../config";
 
 export class DataAPI {
-  static async getUsers(id, stats = null) {
+  static async getUsers(id) {
     try {
       const response = await axios.get(`${BASE_URL}${id}`);
       return response.data;
